1927cc8e39ced7f25cc9cb8cb827879f8b27fe4c
S401-11 <S401-11@DESKTOP-BVOG29I>
2016-08-22 16:19:07 +0900

    1st commit

---
+++ WebContent/hello.js
@@ -0,0 +1,8 @@
+document.write( "Hello World" ); 
+alert("Hello World");
+//window.location.href="http://www.sungkyul.ac.kr";
+document.write("<h1>Hello World</h1>"); //띄우기 
+console.log("hello");
+console.error("hello");
+console.info("hello");
+console.warn("hello");//warning
